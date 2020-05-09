480e3bed8870cc8512e455aff7529683a339beb5
Nayiem Willems <info@hodlercompany.com>
2020-05-09 15:28:30 +0200

    changes

--- public/js/custom.js
+++ public/js/custom.js
@@ -41,35 +41,7 @@ items.forEach(item => item.addEventListener('click', toggleAccordion));
 $("#fakeLoader").fakeLoader();
 // preloader call ends here
 
-/* Javascript code for pricing tables */
-var e = document.getElementById("filt-monthly"),
-    d = document.getElementById("filt-date"),
-    t = document.getElementById("switcher"),
-    m = document.getElementById("monthly"),
-    y = document.getElementById("yearly");
 
-e.addEventListener("click", function() {
-    t.checked = false;
-    e.classList.add("toggler--is-active");
-    d.classList.remove("toggler--is-active");
-    m.classList.remove("hide");
-    y.classList.add("hide");
-});
-
-d.addEventListener("click", function() {
-    t.checked = true;
-    d.classList.add("toggler--is-active");
-    e.classList.remove("toggler--is-active");
-    m.classList.add("hide");
-    y.classList.remove("hide");
-});
-
-t.addEventListener("click", function() {
-    d.classList.toggle("toggler--is-active");
-    e.classList.toggle("toggler--is-active");
-    m.classList.toggle("hide");
-    y.classList.toggle("hide");
-})
 
 // Javascript code for typewriter
 var TxtType = function (el, toRotate, period) {
